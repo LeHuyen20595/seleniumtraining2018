edd28cb1d041467311506eb2a38691244208f3d3
Huyen Le <lehuyen20595@gmail.com>
2018-07-30 09:19:10 +0700

    Commit for topic 6,7

---
+++ WEBDRIVER_API_HUYENLT_02/src/webdriver_api/Topic7_JavaExecutor.java
@@ -0,0 +1,153 @@
+package webdriver_api;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Topic7_JavaExecutor {
+	private static final String WEBDRIVER = "webdriver.chrome.driver";
+	private static final String CHROMEDRIVER = ".\\drivers\\chromedriver.exe";
+	private static final String URL_01 = "http://live.guru99.com/";
+	private static final String URL_02 = "https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_input_disabled";
+	private WebDriver driver;
+	private JavascriptExecutor js;
+	
+	@BeforeTest
+	public void beforeTest() {
+		System.setProperty(WEBDRIVER, CHROMEDRIVER);
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		js = (JavascriptExecutor) driver;
+	}
+
+//	@Test
+//	public void test_01() {
+//		driver.get(URL_01);
+//		
+//		// Get and verify domain
+//		String realDomain = getDomainByJS();
+//		Assert.assertEquals(realDomain, "live.guru99.com");
+//		
+//		// Get and verify page link
+//		String realURL = getCurrentURLByJS();
+//		Assert.assertEquals(realURL, "http://live.guru99.com/");
+//		
+//		// Open Mobile Page
+//		clickButtonByJS(driver.findElement(By.xpath("//div[@id='header-nav']//a")));
+//		
+//		// Add Samsung galaxy to card
+//		clickButtonByJS(driver.findElement(By.xpath("//li[@class='item last']//a[@title='Samsung Galaxy']//following-sibling::div//button")));
+//		
+//		// verify text "Samsung Galaxy was added to your shopping cart." by testNG
+//		String msAddSuccess = driver.findElement(By.xpath("//li[@class='success-msg']//span")).getText();
+//		Assert.assertEquals(msAddSuccess, "Samsung Galaxy was added to your shopping cart.");
+//		
+//		// Verify text by js
+//		if(verifyByJS("Samsung Galaxy was added to your shopping cart.") == true) {
+//			System.out.println("Samsung Galaxy was added to your shopping cart.");
+//		}else {
+//			System.out.println("Add product failed");
+//		}
+//		
+//		// Click on Privacy Policy 
+//		clickButtonByJS(driver.findElement(By.xpath("//li//a[text()='Privacy Policy']")));
+//		
+//		// Verify title page : Privacy Policy
+//		String realTitle = getTitleByJS();
+//		Assert.assertEquals(realTitle, "Privacy Policy");
+//		
+//		// Croll to the bottom of page
+//		scrollToTheBottomOfPage();
+//		
+//		// step 09 verify with just one xpath
+//		
+//		// navigate to "http://demo.guru99.com/v4/"
+//		navigateByJS();
+//		
+//		// verify domain 
+//		Assert.assertEquals(getDomainByJS(), "demo.guru99.com");
+//	}
+	
+	@Test
+	public void test_02_removeAttribute() {
+		driver.get(URL_02);
+		
+		driver.switchTo().frame("iframeResult");
+		removeAttribute(driver.findElement(By.name("lname")));
+		driver.findElement(By.name("lname")).sendKeys("Huyen");
+		driver.findElement(By.xpath("//input[@type='submit']")).click();
+		if(verifyByJS("fname=&lname=Huyen") == true) {
+			System.out.println("Test 02 passed");
+		}else {
+			System.out.println("Test 02 failed");
+		}
+	}
+	
+	public String getDomainByJS() {
+		String domain = "";
+		try {
+			domain = (String) js.executeScript("return document.domain");
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e);
+		}
+		return domain;
+	}
+	
+	public String getCurrentURLByJS() {
+		String currentURL = "";
+		try {
+			currentURL = (String) js.executeScript("return document.URL");
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e);
+		}
+		
+		return currentURL;
+	}
+	
+	public void clickButtonByJS(WebElement element) {
+		js.executeScript("arguments[0].click()", element);
+	}
+	
+	public boolean verifyByJS(String expectMessage) {
+		boolean check = false;
+		String innerText = (String) js.executeScript("return document.documentElement.innerText");
+		if(innerText.contains(expectMessage)) {
+			check = true;
+		}
+		return check;
+	}
+	
+	public String getTitleByJS() {
+		String title = "";
+		try {
+			title = (String) js.executeScript("return document.title");
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e);
+		}
+		
+		return title;
+	}
+	
+	public void scrollToTheBottomOfPage() {
+		js.executeScript("window.scrollBy(0, document.body.scrollHeight)");
+	}
+	
+	public void navigateByJS() {
+		js.executeScript("window.location = 'http://demo.guru99.com/v4/'");
+	}
+	
+	public void removeAttribute(WebElement element) {
+		js.executeScript("arguments[0].removeAttribute('disable');", element);
+	}
+}
--- WEBDRIVER_API_HUYENLT_02/src/webdriver_api/Topic_06_frame.java
+++ WEBDRIVER_API_HUYENLT_02/src/webdriver_api/Topic_06_frame.java
@@ -1,19 +1,21 @@
 package webdriver_api;
 
-import org.testng.annotations.Test;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class Topic_06_frame {
 	private static final String WEBDRIVER = "webdriver.chrome.driver";
 	private static final String CHROMEDRIVER = ".\\drivers\\chromedriver.exe";
 	private static final String URL_01 = "http://www.hdfcbank.com/";
+	private static final String URL_02 = "http://daominhdam.890m.com/";
 	private WebDriver driver;
 
 	@BeforeMethod
@@ -21,32 +23,57 @@ public class Topic_06_frame {
 		System.setProperty(WEBDRIVER, CHROMEDRIVER);
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
 	}
 
+//	@Test
+//	public void test_01_() {
+//		try {
+//			driver.get(URL_01);
+//			if(driver.findElements(By.id("vizury-notification-template")).size() == 1) {
+//				driver.switchTo().frame(driver.findElement(By.id("vizury-notification-template")));
+//				driver.findElement(By.id("div-close")).click();
+//				driver.switchTo().defaultContent();
+//			}
+//			
+//		} catch (Exception e) {
+//			// TODO: handle exception
+//		}
+//	}
+	
 	@Test
-	public void test_01_() {
-		driver.get(URL_01);
-		if (driver.findElement(By.xpath("//iframe[@id='vizury-notification-template']")).isDisplayed()) {
-			driver.switchTo().frame(driver.findElement(By.id("vizury-notification-template")));
-			driver.findElement(By.id("div-close")).click();
-			driver.switchTo().defaultContent();
-		}
-
-		WebElement iFrameFirst = driver.findElement(By.xpath("//div[@class='flipBannerWrap']//iframe"));
-		driver.switchTo().frame(iFrameFirst);
-		Assert.assertEquals(driver.findElement(By.id("messageText")).getText(), "What are you looking for?");
-		driver.switchTo().defaultContent();
-
-		WebElement iFrameSed = driver.findElement(By.xpath("//div[@class='slidingbanners']//iframe"));
-		driver.switchTo().frame(iFrameSed);
-		Assert.assertEquals(driver.findElements(By.xpath("//div[@class='products-list']/div")).size(), 6);
-		driver.switchTo().defaultContent();
-
-		Assert.assertEquals(
-				driver.findElements(By.xpath("//div[@class='flipBanner']/div[contains(@class, 'product')]")).size(), 8);
-		driver.switchTo().defaultContent();
-
+	public void test_02_windowhandle() throws InterruptedException {
+		driver.get(URL_02);
+		String parentWindow = driver.getWindowHandle();
+		System.out.println(parentWindow);
+		driver.findElement(By.xpath("//fieldset/label[@for='open window']/following-sibling::a")).click();
+		Thread.sleep(2000);
+		switchToChildWindow(parentWindow);
+		Assert.assertEquals(driver.getTitle(), "Google");
+		Thread.sleep(1000);
+		driver.close();
+		driver.switchTo().window(parentWindow);
 	}
+	
+	@Test
+	public void test_03_windowhanlde2() {
+		
+		
+	}
+	
+	
+	public void switchToChildWindow(String parentWindow) {
+		Set<String> allWindow = driver.getWindowHandles();
+		for(String childWindow : allWindow) {
+			if(!childWindow.equals(parentWindow)) {
+				driver.switchTo().window(childWindow);
+				break;
+			}
+		}
+	}
+	
+		
+
 
 	@AfterMethod
 	public void after() {
