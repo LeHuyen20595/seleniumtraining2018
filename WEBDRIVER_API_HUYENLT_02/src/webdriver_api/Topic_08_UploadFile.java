d08bbaccb8c05607d41cfa73c4964198368eb90a
Le Huyen <lehuyen20595@gmail.com>
2018-08-03 10:58:34 +0700

    Update for topic 8

--- WEBDRIVER_API_HUYENLT_02/src/webdriver_api/Topic_08_UploadFile.java
+++ WEBDRIVER_API_HUYENLT_02/src/webdriver_api/Topic_08_UploadFile.java
@@ -7,30 +7,32 @@ import java.awt.datatransfer.StringSelection;
 import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import bsh.util.Util;
+import commom.RandomString;
+import commom.Utils;
+
 public class Topic_08_UploadFile {
-	private static final String WEBDRIVER = "webdriver.chrome.driver";
-	private static final String CHROMEDRIVER = ".\\drivers\\chromedriver.exe";
-	private static final String FILEPATH = "C:\\Users\\huyenlet\\git\\seleniumtraining2018Ver2\\WEBDRIVER_API_HUYENLT_02\\fileupload\\1.PNG";
 	private static final String URL_01 = "http://blueimp.github.io/jQuery-File-Upload/";
 	private static final String URL_02 = "https://encodable.com/uploaddemo/";
 	private WebDriver driver;
 	private RandomString rd;
+	private String browser;
 
 	@BeforeMethod
 	public void setUp() {
-		System.setProperty(WEBDRIVER, CHROMEDRIVER);
-		driver = new ChromeDriver();
+		browser = Utils.CHROMEBROWSER;
+		runWithMultiBrowser(browser);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		rd = new RandomString();
@@ -39,7 +41,7 @@ public class Topic_08_UploadFile {
 	@Test
 	public void test_01_uploadFileBySendkeyDirectly() throws InterruptedException {
 		driver.get(URL_01);
-		driver.findElement(By.xpath("//input[@type='file']")).sendKeys(FILEPATH);
+		driver.findElement(By.xpath("//input[@type='file']")).sendKeys(Utils.FILEPATH);
 		Thread.sleep(2000);
 		Assert.assertEquals(driver.findElement(By.xpath("//p[@class='name']")).getText(), "1.PNG");
 	}
@@ -49,15 +51,16 @@ public class Topic_08_UploadFile {
 		driver.get(URL_01);
 		// click on add file button
 		driver.findElement(By.cssSelector(".fileinput-button")).click();
-		Runtime.getRuntime().exec(new String[] { ".\\libs\\chrome.exe", FILEPATH });
+		Runtime.getRuntime().exec(new String[] {setUpBrowserLibForAutoIT(browser), Utils.FILEPATH });
 		Thread.sleep(2000);
 		Assert.assertEquals(driver.findElement(By.xpath("//p[@class='name']")).getText(), "1.PNG");
 	}
 
+
 	@Test
 	public void test_03_uploadFileByRobotClass() throws InterruptedException, AWTException {
 		driver.get(URL_01);
-		StringSelection fileSelect = new StringSelection(FILEPATH);
+		StringSelection fileSelect = new StringSelection(Utils.FILEPATH);
 		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(fileSelect, null);
 		driver.findElement(By.cssSelector(".fileinput-button")).click();
 
@@ -88,7 +91,7 @@ public class Topic_08_UploadFile {
 		driver.get(URL_02);
 
 		// Choose file to upload
-		driver.findElement(By.xpath("//input[@type='file']")).sendKeys(FILEPATH);
+		driver.findElement(By.xpath("//input[@type='file']")).sendKeys(Utils.FILEPATH);
 
 		// Choose subdir folder
 		Select slSubdir = new Select(driver.findElement(By.name("subdir1")));
@@ -126,6 +129,44 @@ public class Topic_08_UploadFile {
 
 		// Verify uploaded file name
 		Assert.assertEquals(driver.findElement(By.xpath("//a[@id='fclink-1PNG']")).getText(), "1.PNG");
+	}
+	
+	public void runWithMultiBrowser(String browser) {
+		switch (browser) {
+		case Utils.CHROMEBROWSER:
+			System.setProperty(Utils.WEBDRIVER, Utils.CHROMEDRIVER);
+			driver = new ChromeDriver();
+			break;
+			
+		case Utils.FIREFOXBROWSER:
+			driver = new FirefoxDriver();
+			break;
+			
+		case Utils.IEBROWSER:
+			System.setProperty(Utils.WEBDRIVER, Utils.IEDRIVER);
+			driver = new InternetExplorerDriver();
+			break;
+
+		default:
+			break;
+		}
+	}
+	
+	public String setUpBrowserLibForAutoIT(String browser) {
+		switch (browser) {
+		case Utils.CHROMEBROWSER:
+			return ".\\libs\\chrome.exe";
+			
+		case Utils.FIREFOXBROWSER:
+			return ".\\libs\\firefox.exe";
+			
+		case Utils.IEBROWSER:
+			return ".\\libs\\ie.exe";
+
+		default:
+			System.out.println("The browser name not found");
+			return null;
+		}
 	}
 
 	public void verifyText(String expectText, String elementName) {
